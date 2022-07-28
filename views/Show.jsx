08f01296d9036ec22f56998aef6d36fa870deda7
Yao Jiang <yaojiang7595@gmail.com>
2022-07-28 14:04:21 -0400

    Created show views of each pokemon

---
+++ views/Show.jsx
@@ -0,0 +1,26 @@
+const React = require("react");
+
+const myStyle = {
+  color: "#ffffff",
+  backgroundColor: "#000000",
+};
+
+class MyFirstComponent extends React.Component {
+  render() {
+    const { pokemon } = this.props;
+    return (
+      <div>
+        <div style={myStyle}>
+          <h1>Gotta Catch 'Em All</h1>
+        </div>
+        <h2>{pokemon.name[0].toUpperCase() + pokemon.name.slice(1)}</h2>
+        <img src={pokemon.img + ".jpg"} />
+        <nav>
+          <a href="/pokemon">Back</a>
+        </nav>
+      </div>
+    );
+  }
+}
+
+module.exports = MyFirstComponent;
